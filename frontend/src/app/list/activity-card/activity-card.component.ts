430ff5d31238f6c521fea5205e3f84e02e0897f5
andy <hotboiii@hotmail.ch>
2019-09-28 01:17:32 +0200

    add some basic cards to listview

---
+++ frontend/src/app/list/activity-card/activity-card.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Activity} from '../activity';
+
+@Component({
+  selector: 'app-activity-card',
+  templateUrl: './activity-card.component.html',
+  styleUrls: ['./activity-card.component.scss'],
+})
+export class ActivityCardComponent implements OnInit {
+
+  @Input()
+  public activity: Activity;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
