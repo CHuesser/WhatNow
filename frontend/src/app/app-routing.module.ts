3a6122ce70756602a0b2acf3b1965776a074e52d
Cloe <cloe.huesser@gmail.com>
2019-09-28 02:40:01 +0200

    service & some activities

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },  { path: 'detail-view', loadChildren: './detail-view/detail-view.module#DetailViewPageModule' }
+
 ];
 
 @NgModule({
---
+++ frontend/src/app/types.ts
@@ -0,0 +1,29 @@
+export interface Activity {
+    event_id: number;
+    date: string;
+    start_time: string;
+    end_time?: any;
+    title_de: string;
+    title_en: string;
+    title_fr: string;
+    title_it: string;
+    short_description_de: string;
+    short_description_en: string;
+    short_description_fr: string;
+    short_description_it: string;
+    long_description_de: string;
+    long_description_en: string;
+    long_description_fr: string;
+    long_description_it: string;
+    homepage?: any;
+    price_information: string;
+    thumbnail_url: string;
+    address_street: string;
+    address_address_line?: any;
+    address_zip: string;
+    address_city: string;
+    address_country: string;
+    address_venue_name: string;
+    address_latitude: number;
+    address_longitude: number;
+}
